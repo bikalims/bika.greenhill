3cdd1f4610cfc787c1087def805048d8820279f2
Lunga Baliwe <lunga001@gmail.com>
2023-06-30 11:00:20 +0200

    Put back client filtering on SamplePoint

--- src/bika/greenhill/monkey/analysisrequest.py
+++ src/bika/greenhill/monkey/analysisrequest.py
@@ -6,61 +6,6 @@ from senaite import api
 from zope.component import getAdapters
 
 
-def get_client_info(self, obj):
-    """Returns the client info of an object
-    """
-    info = self.get_base_info(obj)
-
-    # Set the default contact, but only if empty. The Contact field is
-    # flushed each time the Client changes, so we can assume that if there
-    # is a selected contact, it belongs to current client already
-    default_contact = self.get_default_contact(client=obj)
-    if default_contact:
-        contact_info = self.get_contact_info(default_contact)
-        contact_info.update({"if_empty": True})
-        info["field_values"].update({
-            "Contact": contact_info
-        })
-
-    # Set default CC Email field
-    info["field_values"].update({
-        "CCEmails": {"value": obj.getCCEmails(), "if_empty": True}
-    })
-
-    # UID of the client
-    uid = api.get_uid(obj)
-
-    # catalog queries for UI field filtering
-    filter_queries = {
-        "Contact": {
-            "getParentUID": [uid]
-        },
-        "CCContact": {
-            "getParentUID": [uid]
-        },
-        "InvoiceContact": {
-            "getParentUID": [uid]
-        },
-        "Template": {
-            "getClientUID": [uid, ""],
-        },
-        "Profiles": {
-            "getClientUID": [uid, ""],
-        },
-        "Specification": {
-            "getClientUID": [uid, ""],
-        },
-        "Sample": {
-            "getClientUID": [uid],
-        },
-        "Batch": {
-            "getClientUID": [uid, ""],
-        }
-    }
-    info["filter_queries"] = filter_queries
-    return info
-
-
 def get_sampletype_info(self, obj):
     """Returns the info for a Sample Type
     """
@@ -126,6 +71,7 @@ def ajax_get_flush_settings(self):
             "Contact",
             "CCContact",
             "InvoiceContact",
+            "SamplePoint",
             "Template",
             "Profiles",
             "PrimaryAnalysisRequest",
